a39a792d7df1ab7107988d62715e28ec429ff6b5
Ayush Barman <barmanayush2980@gmail.com>
2025-01-12 04:06:12 +0530

    Migrated Codebase and Completed memory service with chat feature

---
+++ app/api/chat/[chatId]/route.ts
@@ -0,0 +1,141 @@
+import dotenv from "dotenv";
+import { StreamingTextResponse, LangChainStream } from "ai";
+import { auth, currentUser } from "@clerk/nextjs";
+import { CallbackManager } from "langchain/callbacks";
+import { NextResponse } from "next/server";
+
+import Replicate from "replicate"; // Use Replicate's official SDK
+import { MemoryManager } from "@/lib/memory";
+import { ratelimit } from "@/lib/rate-limit";
+import prismadb from "@/lib/prismadb";
+
+dotenv.config({ path: `.env` });
+
+export async function POST(
+  request: Request,
+  { params }: { params: { chatId: string } }
+) {
+  try {
+    const { prompt } = await request.json();
+    const user = await currentUser();
+
+    if (!user || !user.firstName || !user.id)
+      return new NextResponse("Unauthorized!", { status: 401 });
+
+    const identifier = request.url + "-" + user.id;
+    const { success } = await ratelimit(identifier);
+
+    if (!success)
+      return new NextResponse("Ratelimit Exceeded!", { status: 429 });
+
+    const companion = await prismadb.companion.update({
+      where: { id: params.chatId },
+      data: {
+        messages: {
+          create: {
+            content: prompt,
+            role: "user",
+            userId: user.id,
+          },
+        },
+      },
+    });
+
+    if (!companion)
+      return new NextResponse("Companion Not Found.", { status: 404 });
+
+    const name = companion.id;
+    const companion_file_name = name + ".txt";
+
+    const companionKey = {
+      companionName: name,
+      userId: user.id,
+      modelName: "llama2-13b",
+    };
+
+    const memoryManager = await MemoryManager.getInstance();
+
+    const records = await memoryManager.readLatestHistory(companionKey);
+
+    if (records.length === 0)
+      await memoryManager.seedChatHistory(companion.seed, "\n\n", companionKey);
+
+    await memoryManager.writeToHistory("User: " + prompt + "\n", companionKey);
+    console.log("Hola Amigo");
+
+    const recentChatHistory = await memoryManager.readLatestHistory(
+      companionKey
+    );
+
+    const similarDocs = await memoryManager.vectorSearch(
+      recentChatHistory,
+      companion_file_name
+    );
+
+    let relevantHistory = "";
+
+    if (!!similarDocs && similarDocs.length !== 0)
+      relevantHistory = similarDocs.map((doc) => doc.pageContent).join("\n");
+
+    const replicate = new Replicate({
+      auth: process.env.REPLICATE_API_TOKEN || "",
+    });
+
+    console.log("Sending response to Replicate !")   
+    
+    const response = await replicate.run(
+      "a16z-infra/llama-2-13b-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5",
+      {
+        input: {
+          max_length: 2048,
+          prompt: `
+            ONLY generate plain sentences without prefix of who is speaking. DO NOT use ${companion.name}: prefix. 
+            
+            ${companion.instructions}
+            
+            Below are relevant details about ${companion.name}'s past and the conversation you are in.
+            ${relevantHistory}
+            
+            ${recentChatHistory}\n${companion.name}:`,
+          },
+        }
+      );
+      console.log("received response from Replicate !" , response)   
+      
+      const cleaned = String(response).replaceAll(",", "");
+      const chunks = cleaned.split("\n");
+      const finalResponse = chunks[0];
+      
+      await memoryManager.writeToHistory("" + finalResponse.trim(), companionKey);
+
+    var Readable = require("stream").Readable;
+
+    let stream = new Readable();
+    stream.push(finalResponse);
+    stream.push(null);
+
+    if (finalResponse !== undefined && finalResponse.length > 1) {
+      memoryManager.writeToHistory("" + finalResponse.trim(), companionKey);
+
+      await prismadb.companion.update({
+        where: {
+          id: params.chatId,
+        },
+        data: {
+          messages: {
+            create: {
+              content: finalResponse.trim(),
+              role: "system",
+              userId: user.id,
+            },
+          },
+        },
+      });
+    }
+
+    return new StreamingTextResponse(stream);
+  } catch (error) {
+    console.error("[CHAT_POST]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
---
+++ lib/memory.ts
@@ -0,0 +1,120 @@
+import { Redis } from "@upstash/redis";
+import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
+import { Pinecone } from "@pinecone-database/pinecone";
+import { PineconeStore } from "@langchain/pinecone";
+import { Document } from "@langchain/core/documents";
+
+export type CompanionKey = {
+  companionName: string;
+  modelName: string;
+  userId: string;
+};
+
+export class MemoryManager {
+  private static instance: MemoryManager;
+  private history: Redis;
+  private vectorDBClient: Pinecone;
+
+  public constructor() {
+    this.history = Redis.fromEnv();
+    this.vectorDBClient = new Pinecone({
+      apiKey: process.env.PINECONE_API_KEY!,
+    });
+  }
+
+  public async init() {
+  }
+
+  public async vectorSearch(
+    recentChatHistory: string,
+    companionFileName: string
+  ): Promise<Document<Record<string, any>>[]> {
+    const pineconeIndex = this.vectorDBClient.Index(
+      process.env.PINECONE_INDEX! || "companion"
+    );
+
+    const embeddings = new GoogleGenerativeAIEmbeddings({
+      apiKey: process.env.GOOGLE_API_KEY!,
+      modelName: "embedding-001"
+    });
+
+    const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
+      pineconeIndex,
+      namespace: companionFileName,
+    });
+
+    const similarDocs = await vectorStore
+      .similaritySearch(recentChatHistory, 3)
+      .catch((err) => {
+        console.error("Failed to Get Vector Search Results.", err);
+        return [];
+      });
+
+    return similarDocs;
+  }
+
+  // Rest of the class remains the same...
+  public static async getInstance(): Promise<MemoryManager> {
+    if (!MemoryManager.instance) {
+      MemoryManager.instance = new MemoryManager();
+      await MemoryManager.instance.init();
+    }
+    return MemoryManager.instance;
+  }
+
+  private generateRedisCompanionKey(companionKey: CompanionKey): string {
+    return `${companionKey.companionName}-${companionKey.modelName}-${companionKey.userId}`;
+  }
+
+  public async writeToHistory(text: string, companionKey: CompanionKey) {
+    if (!companionKey || typeof companionKey.userId == "undefined") {
+      console.error("Companion Key Set Incorrectly!");
+      return "";
+    }
+
+    const key = this.generateRedisCompanionKey(companionKey);
+    const result = await this.history.zadd(key, {
+      score: Date.now(),
+      member: text
+    });
+
+    return result;
+  }
+
+  public async readLatestHistory(companionKey: CompanionKey): Promise<string> {
+    if (!companionKey || typeof companionKey.userId == "undefined") {
+      console.error("Companion Key Set Incorrectly!");
+      return "";
+    }
+
+    const key = this.generateRedisCompanionKey(companionKey);
+    let result = await this.history.zrange(key, 0, Date.now(), {
+      byScore: true
+    });
+
+    result = result.slice(-30).reverse();
+    const recentChats = result.reverse().join("\n");
+    return recentChats;
+  }
+
+  public async seedChatHistory(
+    seedContent: String,
+    delimiter: string = "\n",
+    companionKey: CompanionKey
+  ) {
+    const key = this.generateRedisCompanionKey(companionKey);
+
+    if (await this.history.exists(key)) {
+      console.log("User Already Has Chat History.");
+      return;
+    }
+
+    const content = seedContent.split(delimiter);
+    let counter = 0;
+
+    for (const line of content) {
+      await this.history.zadd(key, { score: counter, member: line });
+      counter += 1;
+    }
+  }
+}
