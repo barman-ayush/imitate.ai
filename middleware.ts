911f3574e5e2ccdbe69ba4b315d598b678d1222a
Ayush Barman <barmanayush2980@gmail.com>
2025-01-13 17:46:07 +0530

    Minor Changes

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,13 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({ publicRoutes: ["/api/webhook"] });
+export default authMiddleware({
+  publicRoutes: ["/api/webhook", "/"], debug: true,
+});
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"]
+  // matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"]
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
+  ],
 };
